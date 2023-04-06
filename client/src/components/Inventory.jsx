a8d5b4195604d585d86bb3673a75a3f37842b867
alkumeidy <p.alkumeidy@gmail.com>
2023-04-06 11:09:43 -0600

    Added bootstrap table.

--- client/src/components/Inventory.jsx
+++ client/src/components/Inventory.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
 
 function Inventory() {
     const [inventory, setInventory] = useState([]);
@@ -8,21 +9,36 @@ function Inventory() {
             method: "GET",
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
-              },
-          })
+            },
+        })
             .then((res) => res.json())
             .then((data) => setInventory(data))
-      }, []);
+    }, []);
 
-      const inventoryArray = inventory.map((item) => <li>{item.item_name}</li>)
+    const inventoryArray = inventory.map((item) => <tr key={item.id}><td>{item.id}</td><td>{item.UserId}</td><td>{item.item_name}</td><td>{item.description}</td><td>{item.quantity}</td><td><input type="button" value="Delete" /></td></tr> )
 
     return (
         <>
-        <ul>
-            {inventoryArray}
-        </ul>
+            <input type="text" value="Enter item name" />
+            <input type="text" value="Enter quantity" />
+            <input type="text" value="Enter description" />
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Assigned To</th>
+                        <th>Item Name</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventoryArray}
+                </tbody>
+            </Table>
         </>
     );
-  }
-  
-  export default Inventory;
+}
+
+export default Inventory;
